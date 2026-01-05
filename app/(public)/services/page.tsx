139d2cfd43bdfc392f948b89df67ff8221db7d54
adewale <adewale.d.a@outlook.com>
2026-01-05 20:53:33 +0100

    services header text mobile update

--- app/(public)/services/page.tsx
+++ app/(public)/services/page.tsx
@@ -21,7 +21,7 @@ export default function Services() {
     <div className=" flex flex-col items-center  pt-28">
       <section className="w-full flex flex-col gap-5  justify-center items-center">
         <div className="w-full pt-10 lg:pt-16 pb-20 flex max-w-7xl px-5 lg:px-0 flex-col justify-center items-center gap-8">
-          <h2 className="text-6xl lg:text-7xl font-bold text-transparent bg-linear-45 from-secondary via-primary to-secondary dark:from-primary dark:via-[#eda6a9] dark:to-primary bg-clip-text">
+          <h2 className="text-5xl lg:text-7xl text-center font-bold text-transparent bg-linear-45 from-secondary via-primary to-secondary dark:from-primary dark:via-[#eda6a9] dark:to-primary bg-clip-text">
             Our Services Pillars
           </h2>
           <p className=" text-center max-w-xl text-lg">
